b2e8200f6f594b417045ab6e608e1d60ce7e9e94
YMaiNgoc <ymaingoc629@gmail.com>
2025-05-09 22:14:41 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentProfileController;
 use App\Http\Controllers\Api\GoalController;
 
 Route::apiResource('goals', GoalController::class);
+Route::get('student-profile/{id}', [StudentProfileController::class, 'show']); 
+Route::put('student-profile/{id}', [StudentProfileController::class, 'update']); 
+Route::post('student-profile', [StudentProfileController::class, 'store']);    
+Route::delete('student-profile/{id}', [StudentProfileController::class, 'destroy']);
+// Route::get('student-profile/{id}', [StudentProfileController::class, 'show']);
+// Route::put('student-profile/{id}', [StudentProfileController::class, 'update']);
