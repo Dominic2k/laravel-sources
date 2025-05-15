bd5d1886b0facd51f055867a2aa3528b254ebb85
CED-b2005 <duqingping2k5@gmail.com>
2025-05-15 14:41:33 +0700

    modify migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,29 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // User::factory(10)->create();
+
         User::factory()->create([
-            'full_name' => 'Test User',
+            'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => Hash::make("1234")
         ]);
-
-        for($i = 1; $i <= 10; $i++) {
-            if ($i <= 5) {
-                User::factory()->create([ 
-                    'full_name' => 'student_'.$i,
-                    'email' => 'student_'.$i.'@pnv.com',
-                    'password' => Hash::make("1234"),
-                    'role' => 'student'
-                ]);
-            } else {
-                User::factory()->create([ 
-                    'full_name' => 'teacher_'.$i,
-                    'email' => 'teacher_'.$i.'@pnv.com',
-                    'password' => Hash::make("1234"),
-                    'role' => 'teacher'
-                ]);
-            }
-
-        }
     }
 }
